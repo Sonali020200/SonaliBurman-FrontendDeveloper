185608762ad1a70d92861885dfe1b04d362fd48c
Sonali Burman <sonali.b.020200@gmail.com>
2024-06-11 22:11:59 +0530

    done

--- AI.GEN/src/components/HowItWorks.jsx
+++ AI.GEN/src/components/HowItWorks.jsx
@@ -1,44 +1,82 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import avatar from "../../src/assets/Avatar.png";
 import '../styles/HowItWorks.css';
 
 export const HowItWorks = () => {
-    useEffect(() => {
-      const paragraphs = document.querySelectorAll(".animated-paragraph");
-      let current = 0;
-
-      const updateParagraphs = () => {
-        paragraphs.forEach((paragraph, index) => {
-          if (index === current) {
-            paragraph.classList.add("text-active");
-            paragraph.classList.remove("text-inactive");
-          } else {
-            paragraph.classList.remove("text-active");
-            paragraph.classList.add("text-inactive");
-          }
-        });
-        current = (current + 1) % paragraphs.length;
-      };
-
-      updateParagraphs(); 
-      const interval = setInterval(updateParagraphs, 900);
-
-      return () => clearInterval(interval); 
-    }, []);
-
+   
     return (
-      <div className="avatar-section md:flex justify-start items-center gap-10 relative">
-        <div className="md:w-[60%] md:rounded-r-lg rounded-lg overflow-hidden">
-          <img src={avatar} alt="avatar-ai" className="overflow-hidden" />
+      <>
+        <div className="flex flex-col items-center justify-center gap-[32px] max-w-full z-[2] ">
+          <div className="self-stretch flex flex-col items-start justify-start max-w-full">
+            <div className="self-stretch flex flex-col items-start justify-center py-0 pr-[26px] pl-0 box-border relative max-w-full">
+              <div className="self-stretch flex flex-col items-start justify-start">
+                <img
+                  className="w-full h-full object-contain absolute left-[0px] top-[13px] [transform:scale(1.406)]"
+                  src={avatar}
+                  alt="girl"
+                />
+                <div className="w-[96.81%] !m-[0] absolute right-[3.19%] bottom-[0px] left-[0%] flex flex-col items-start justify-center max-w-full">
+                  <div className="self-stretch flex flex-col items-start justify-start"></div>
+                </div>
+                <div className="self-stretch h-32 flex flex-col items-start justify-start">
+                  <div className="self-stretch flex-1" />
+                </div>
+                <div className="self-stretch h-32 flex flex-col items-start justify-start">
+                  <div className="self-stretch flex-1" />
+                </div>
+              </div>
+              <div className="self-stretch flex flex-col items-start justify-center max-w-full z-[1] text-[64px] text-white">
+                <div className="self-stretch flex flex-col items-start justify-start max-w-full">
+                  <div className="self-stretch flex flex-col items-start justify-start py-0 pr-5 pl-0 box-border max-w-full">
+                    <h1 className="m-0 w-[500px] h-16 relative text-inherit leading-[64px] font-semibold font-inherit flex items-center max-w-full mq450:text-19xl mq450:leading-[38px] mq825:text-[51px] mq825:leading-[51px]">
+                      How it works
+                    </h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="self-stretch flex flex-col items-start justify-start max-w-full">
+            <div className="self-stretch overflow-hidden flex flex-col items-start justify-center py-0 pr-[26px] pl-0 box-border relative min-h-[256px] max-w-full">
+              <div className="w-[96.81%] !m-[0] absolute right-[3.19%] bottom-[0px] left-[0%] flex flex-col items-start justify-center max-w-full">
+                <div className="self-stretch flex flex-col items-start justify-start">
+                  <div className="self-stretch flex flex-col items-start justify-start py-0 pr-5 pl-0">
+                    <h1 className="m-0 self-stretch h-16 relative text-inherit leading-[64px] font-normal font-inherit flex items-center mq450:text-10xl mq450:leading-[38px] mq825:text-19xl mq825:leading-[51px]">
+                      Select an Avatar
+                    </h1>
+                  </div>
+                </div>
+                <div className="self-stretch flex flex-col items-start justify-start">
+                  <div className="self-stretch flex flex-col items-start justify-start py-0 pr-5 pl-0">
+                    <h1 className="m-0 self-stretch h-16 relative text-inherit leading-[64px] font-normal font-inherit flex items-center mq450:text-10xl mq450:leading-[38px] mq825:text-19xl mq825:leading-[51px]">
+                      Create or Generate Script
+                    </h1>
+                  </div>
+                </div>
+                <div className="self-stretch flex flex-col items-start justify-start">
+                  <div className="self-stretch flex flex-col items-start justify-start py-0 pr-5 pl-0">
+                    <h1 className="m-0 self-stretch h-16 relative text-inherit leading-[64px] font-normal font-inherit flex items-center mq450:text-10xl mq450:leading-[38px] mq825:text-19xl mq825:leading-[51px]">
+                      Select AI Voices
+                    </h1>
+                    <h1 className="m-0 self-stretch h-16 relative text-inherit leading-[64px] font-normal font-inherit flex items-center mq450:text-10xl mq450:leading-[38px] mq825:text-19xl mq825:leading-[51px]">
+                      Publish
+                    </h1>
+                  </div>
+                </div>
+              </div>
+              <div className="self-stretch flex flex-col items-start justify-center z-[1]">
+                <div className="self-stretch flex flex-col items-start justify-start">
+                  <div className="self-stretch flex flex-col items-start justify-start py-0 pr-[15px] pl-0">
+                    <h1 className="m-0 self-stretch h-16 relative text-inherit leading-[64px] font-normal font-inherit text-transparent !bg-clip-text [background:linear-gradient(90deg,_#5cc3fa,_#9e6ee6)] [-webkit-background-clip:text] [-webkit-text-fill-color:transparent] flex items-center mq450:text-10xl mq450:leading-[38px] mq825:text-19xl mq825:leading-[51px]">
+                      Select an Avatar
+                    </h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="avatar-section-para">
-          <h1 className="md:text-7xl text-3xl mt-5 font-semibold">How it works</h1>
-          <p className="animated-paragraph text-5xl mt-10 font-bold">Select an Avatar</p>
-          <p className="animated-paragraph text-4xl mt-3 font-bold">Create or Generate Script</p>
-          <p className="animated-paragraph text-4xl mt-3 font-bold">Select AI Voices</p>
-          <p className="animated-paragraph text-4xl mt-3 font-bold">Publish</p>
-        </div>
-      </div>
+      </>
     );
   };
  
