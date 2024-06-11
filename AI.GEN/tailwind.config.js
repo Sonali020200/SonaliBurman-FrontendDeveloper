96881addd9430a61a0418d7369d8634aaaeaa567
Sonali Burman <sonali.b.020200@gmail.com>
2024-06-12 02:06:51 +0530

    edited

--- AI.GEN/tailwind.config.js
+++ AI.GEN/tailwind.config.js
@@ -13,6 +13,20 @@ export default {
       backgroundImage: {
         'custom-radial': 'radial-gradient(50% 50% at 50% 50%, rgba(92, 195, 250, 0.28) 0%, rgba(92, 195, 250, 0) 100%)',
       },
+      keyframes: {
+        'left-right': {
+          '0%, 100%': { transform: 'translateX(0)' },
+          '50%': { transform: 'translateX(50px)' },
+        },
+        'right-left': {
+          '0%, 100%': { transform: 'translateX(0)' },
+          '50%': { transform: 'translateX(-50px)' },
+        },
+      },
+      animation: {
+        'left-right': 'left-right 5s infinite',
+        'right-left': 'right-left 5s infinite',
+      },
 
     },
   },
