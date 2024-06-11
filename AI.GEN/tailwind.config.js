62f1dcff742b26a39b892851abe6883ca0e621fd
Sonali Burman <sonali.b.020200@gmail.com>
2024-06-11 13:02:26 +0530

    done

--- AI.GEN/tailwind.config.js
+++ AI.GEN/tailwind.config.js
@@ -7,6 +7,9 @@ export default {
         primary: "#1DA1F2",
         secondary: "#14171A",
       },
+      fontFamily:{
+        outfit : ["Outfit", "sans-serif"]
+      },
       backgroundImage: {
         'custom-radial': 'radial-gradient(50% 50% at 50% 50%, rgba(92, 195, 250, 0.28) 0%, rgba(92, 195, 250, 0) 100%)',
       },
