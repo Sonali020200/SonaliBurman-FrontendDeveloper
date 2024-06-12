0e022d1275d4f931d1718e9eb453f0cb0d7b7898
Sonali Burman <sonali.b.020200@gmail.com>
2024-06-13 02:23:52 +0530

    done

--- AI.GEN/src/components/Tokenomics.jsx
+++ AI.GEN/src/components/Tokenomics.jsx
@@ -42,41 +42,43 @@ const Tokenomics = () => {
             <div className='flex items-center justify-between lg:px-16'>
                 <img className='w-44 lg:w-auto pl-4' src={Donut} alt="" />
                 <div className='lg:mr-40'>
-                    <div className='w-44 lg:w-full flex border-1 border-gray-200  rounded-lg m-2'>
-                        <span className='w-2 lg:w-6 bg-[#BE2C2C] dir-ltr rounded-s-lg '></span>
-                        <div className='lg:pr-12 lg:py-2 flex text-xs lg:text-xl gap-2 lg:gap-8 m-2'>
-                            <span className='lg:w-36 font-semibold'>Partnership</span>
-                            <span>:</span>
-                            <span>5% tokens</span>
-                        </div>
-                    </div>
-                    <div className='w-44 lg:w-full flex border-1 border-gray-200  rounded-lg m-2'>
-                        <span className='w-2 lg:w-6 bg-[#FF8F34] dir-ltr rounded-s-lg '></span>
-                        <div className='lg:pr-12 lg:py-2 flex text-xs lg:text-xl gap-2 lg:gap-8 m-2'>
-                            <span className='lg:w-36 font-semibold'>LP Pool</span>
-                            <span>:</span>
-                            <span>35% tokens</span>
-                        </div>
-                    </div>
-                    <div className='w-44 lg:w-full flex border-1 border-gray-200  rounded-lg m-2'>
-                        <span className='w-2 lg:w-6 bg-[#9934FF] dir-ltr rounded-s-lg '></span>
-                        <div className='lg:pr-12 lg:py-2 flex text-xs lg:text-xl gap-2 lg:gap-8 m-2'>
-                            <span className='lg:w-36 font-semibold'>Burned</span>
-                            <span>:</span>
-                            <span>30% tokens</span>
-                        </div>
-                    </div>
-                    <div className='w-44 lg:w-full flex border-1 border-gray-200  rounded-lg m-2'>
-                        <span className='w-2 lg:w-6 bg-[#028DFD] dir-ltr rounded-s-lg '></span>
-                        <div className='lg:pr-12 lg:py-2 flex text-xs lg:text-xl gap-2 lg:gap-8 m-2'>
-                            <span className='lg:w-36 font-semibold'>BNB LP Pool</span>
-                            <span>:</span>
-                            <span>30% tokens</span>
-                        </div>
-                    </div>
+                <div className='w-44 lg:w-full flex border border-gray-200 rounded-lg m-2'>
+                <span className='w-2 lg:w-6 bg-[#BE2C2C] dir-ltr rounded-s-lg'></span>
+                 <div className='lg:pr-12 lg:py-2 flex text-xs lg:text-xl gap-2 lg:gap-8 m-2 items-center'>
+                <span className='lg:w-36 font-semibold'>Partnership</span>
+                <span>:</span>
+                <span>5% tokens</span>
+           </div>
+        </div>
+            <div className='w-44 lg:w-full flex border border-gray-200 rounded-lg m-2'>
+            <span className='w-2 lg:w-6 bg-[#FF8F34] dir-ltr rounded-s-lg'></span>
+            <div className='lg:pr-12 lg:py-2 flex text-xs lg:text-xl gap-2 lg:gap-8 m-2 items-center'>
+            <span className='lg:w-36 font-semibold'>LP Pool</span>
+            <span>:</span>
+            <span>35% tokens</span>
+         </div>
+    </div>
+            <div className='w-44 lg:w-full flex border border-gray-200 rounded-lg m-2'>
+            <span className='w-2 lg:w-6 bg-[#9934FF] dir-ltr rounded-s-lg'></span>
+            <div className='lg:pr-12 lg:py-2 flex text-xs lg:text-xl gap-2 lg:gap-8 m-2 items-center'>
+            <span className='lg:w-36 font-semibold'>Burned</span>
+            <span>:</span>
+            <span>30% tokens</span>
+        </div>
+    </div>
+        <div className='w-44 lg:w-full flex border border-gray-200 rounded-lg m-2'>
+        <span className='w-2 lg:w-6 bg-[#028DFD] dir-ltr rounded-s-lg'></span>
+        <div className='lg:pr-12 lg:py-2 flex text-xs lg:text-xl gap-2 lg:gap-8 m-2 items-center'>
+            <span className='lg:w-36 font-semibold'>BNB LP Pool</span>
+            <span>:</span>
+            <span>30% tokens</span>
+        </div>
+    </div>
+</div>
+
                 </div>
             </div>
-        </div>
+        
     
   );
 };
