4eb684c7cf8db1b70b13062a94a1900c2ae01e07
Sonali Burman <sonali.b.020200@gmail.com>
2024-06-13 02:06:45 +0530

    done

--- AI.GEN/src/components/HeroSection.jsx
+++ AI.GEN/src/components/HeroSection.jsx
@@ -23,7 +23,7 @@ export const HeroSection = () => {
   }, []);
 
   return (
-    <section className="hero2">
+    <section className="hero2 mx-auto w-full relative overflow-hidden bg-gradient-to-b from-[#09012a] to-[#0a0a18] to-70%">
       <div className="hero2-first">
         <div ref={textRef}>
           <h2 className='img-animate-down-up animation2-down-up'><span>Create, customize, and publish</span> your digital persona to life effortlessly.</h2>
